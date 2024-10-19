1c24d993fe88b5278de8acaba977fc89e76a1d21
daa201 <139944607+daa201@users.noreply.github.com>
2024-10-19 08:27:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Các chị em bé';
 const giftUrl = 'https://www.facebook.com/PandaCute.Official/';
-const eventName = 'Chúc 20-10 của';
+const eventName = 'Chúc 20-10 tới';
 const titleCard = 'Tặng các bé iu của anh';
 const contentCard = 'Chúc các honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các em trong hôm nay và cả những ngày sau';
 
